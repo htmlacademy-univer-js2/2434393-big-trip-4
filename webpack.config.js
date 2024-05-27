2f9d33391291ed3a474ac6fca07f1acbab3577b3
Alexander Bogdan <bogdan.sanyaa@mail.ru>
2024-05-27 19:25:31 +0500

    Пришёл, увидел, загрузил (часть 2)

--- webpack.config.js
+++ webpack.config.js
@@ -1,47 +1,30 @@
-const path = require('path');
-const CopyPlugin = require('copy-webpack-plugin');
-const HtmlPlugin = require('html-webpack-plugin');
+const path = require("path");
+const CopyPlugin = require("copy-webpack-plugin");
 
 module.exports = {
-  entry: './src/main.js',
+  entry: "./src/main.js",
   output: {
-    filename: 'bundle.[contenthash].js',
-    path: path.resolve(__dirname, 'build'),
-    clean: true
+    path: path.resolve(__dirname, "build"),
+    filename: "bundle.js",
+    clean: true,
   },
-  devtool: 'source-map',
+  devtool: "source-map",
   plugins: [
-    new HtmlPlugin({
-      template: 'public/index.html',
+    new CopyPlugin({
+      patterns: [{ from: "public" }],
     }),
-    new CopyPlugin ({
-      patterns: [
-        {
-          from: 'public',
-          globOptions: {
-            ignore: ['**/index.html']
-          }
-        }
-      ]
-    }
-    )
   ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
-        use: {
-          loader: 'babel-loader',
-          options: {
-            presets: ['@babel/preset-env'],
-          },
-        },
+        use: ["babel-loader"],
       },
       {
         test: /\.css$/i,
         use: ["style-loader", "css-loader"],
       },
-    ]
+    ],
   },
 };
